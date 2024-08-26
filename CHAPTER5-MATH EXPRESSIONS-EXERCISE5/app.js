fc654f680d1a5eddaa01b6450e6d8dec9d892f31
Farah Karamat <farahkaramat@gmail.com>
2024-08-27 02:22:53 +0500

    Javascript chapter 4-5
    Exercises 4- 5

---
+++ CHAPTER5-MATH EXPRESSIONS-EXERCISE5/app.js
@@ -0,0 +1,209 @@
+/*Q.1. Write a program that take two numbers & add them in a
+new variable. Show the result in your browser.*/
+//Answer:
+/*let a = 3;
+let b = 5;
+var c = a + b;
+document.write ("Sum of "+a+" and "+b+" is "+c);*/
+
+/*Q.2. Repeat task1 for subtraction, multiplication, division &
+modulus.*/
+//Answer:
+/*let a = 3;
+let b = 5;
+var c = a - b;
+document.write ("Substraction of "+a+" and "+b+" is "+c);*/
+
+/*let a = 3;
+let b = 5;
+var c = a * b;
+document.write ("Multiplication of "+a+" and "+b+" is "+c);*/
+
+/*let a = 3;
+let b = 5;
+var c = a / b;
+document.write ("Division of "+a+" and "+b+" is "+c);*/
+
+/*let a = 3;
+let b = 5;
+var c = (a / b) *100;
+document.write ("Percentage of "+a+" and "+b+" is "+c);*/
+
+/*Q.3. Do the following using JS Mathematic Expressions
+a. Declare a variable.
+b. Show the value of variable in your browser like “Value
+after variable declaration is: ??”.
+c. Initialize the variable with some number.
+d. Show the value of variable in your browser like “Initial
+value: 5”.
+e. Increment the variable.
+f. Show the value of variable in your browser like “Value
+after increment is: 6”.
+g. Add 7 to the variable.
+h. Show the value of variable in your browser like “Value after addition is: 13”.
+i. Decrement the variable.
+j. Show the value of variable in your browser like “Value
+after decrement is: 12”.
+k. Show the remainder after dividing the variable’s value
+by 3.
+l. Output : “The remainder is : 0”.*/
+//Answer:
+//a and b).
+/*let mynumber = 10 + 5;
+document.write("Value after variable declaration is: " + mynumber);*/
+
+//c)
+/*var num1;
+num1 = 14;
+var num2;
+num2 = num1;
+document.write ("Number-2 \n" +num2+  "Number-1 \n"  +num1)*/
+
+//d)
+/*let mynumber = 5;
+document.write("Initial value: " + mynumber);*/
+
+//e)
+/*var num = 10;
+alert(num)
+num = num + 1;
+alert(num)
+num = num + 1;
+alert(num)
+num = num + 1;
+alert(num)
+num = num + 1;*/
+
+//f)
+/*var num = 5;
+num = num + 1;
+
+document.write("Value after increment is: " +num);*/
+
+//g)
+/*var num = 5;
+num++;
+num++;
+num++;
+num++;
+num++;
+num++;
+num++;
+alert(num)*/
+
+//h)
+/*let myNumber = 6;
+myNumber += 7;
+document.write("Value after addition is:" +myNumber)*/
+
+//i)
+/*let myNumber = 13;
+myNumber--;
+alert(myNumber);*/
+
+//j)
+/*let myNumber = 13;
+myNumber--;
+document.write ("Value after decrement is:" +myNumber)*/
+
+//k)
+/*let myNumber = 12;
+myNumber = 12/3;
+alert(myNumber)*/
+
+//l)
+/*let myNumber = 12;
+let remainder = myNumber % 3;
+alert(remainder);*/
+
+/*Q.4. Cost of one movie ticket is 600 PKR. Write a script to
+store
+ticket price in a variable & calculate the cost of buying 5
+tickets
+to a movie. Example output:*/
+//Answer:
+/*var number = 600;
+number = 600*5;
+document.write ("Total cost to buy 5 tickets to a movie is: " +number+ "PKR")*/
+
+/*Q.5.Write a script to display multiplication table of any
+number in your browser. E.g 4*1=4 ...*/
+//Answer:
+/*function table(number){
+for(var i=1;i<=10;i++){
+var res = i*number;
+document.write (`${number }  x  ${i} =  ${res} \n`);
+
+}
+}
+table(4);*/
+
+/*Q.6. The Temperature Converter: It’s hot out! Let’s make a
+converter based on the steps here.
+a. Store a Celsius temperature into a variable.
+b. Convert it to Fahrenheit & output “NNoC is NNoF”.
+c. Now store a Fahrenheit temperature into a variable.
+d. Convert it to Celsius & output “NNoF is NNoC”.*/
+//Answer:
+
+
+/*Q.7. Write a program to implement checkout process of a
+shopping cart system for an e-commerce website. Store
+the following in variables.
+Price of item 1
+b. Price of item 2
+c. Ordered quantity of item 1
+d. Ordered Quantity of item 2
+e. Shipping charges
+Compute the total cost & show the receipt in your browser.*/
+//Answer: IN HTML FILE
+
+/*Q.8. Store total marks & marks obtained by a student in 2
+variables. Compute the percentage & show the result in
+your browser.*/
+//Answer:
+
+/*var totalmarks = 980;
+var marksobtained = 804;
+var result =  804 *100 / 980;
+document.write ("Total marks:" +totalmarks);
+document.write ("Marks obtained:" +marksobtained);
+document.write ("Percenrage:" + result);*/
+
+/*Q.9. Assume we have 10 US dollars & 25 Saudi Riyals. Write a
+script to convert the total currency to Pakistani Rupees.
+Perform all calculations in a single expression.
+(Exchange rates : 1 US Dollar = 104.80 Pakistani Rupee
+and 1 Saudi Riyal = 28 Pakistani Rupee)*/
+//Anser: IN HTML FILE
+
+/*Q.10. Write a program to initialize a variable with some
+number and do arithmetic in following sequence:
+a. Add 5
+b. Multiply by 10
+c. Divide the result by 2
+Perform all calculations in a single expression*/
+
+/*let number = 8;
+let result = ((number + 5) * 10) / 2;
+alert(result)*/
+
+/*Q.11. The Age Calculator: Forgot how old someone is?
+Calculate it!
+a. Store the current year in a variable.
+b. Store their birth year in a variable.
+c. Calculate their 2 possible ages based on the stored
+values.
+
+Output them to the screen like so: “They are either NN or NN
+years old”.*/
+//Answer:
+/*let currentYear = 2024;
+let birthYear = 1992;
+
+let age1 = currentYear - birthYear;
+let age2 = age1 - 1;
+
+document.write("They are either " +age1+ " and " +age2+ "years old.")*/
+
+/*Q.12.
